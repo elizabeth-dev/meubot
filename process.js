fe850296a85700de93683bb503ca0a2e6029aa14
Elizabeth Martín Campos <eli@zabeth.es>
2019-01-15 20:14:07 +0100

    PM2

---
+++ process.js
@@ -0,0 +1,30 @@
+require('dotenv')
+
+module.exports = {
+	apps: [{
+		name: "meubot",
+		script: "./index.js",
+		watch: true,
+		wait_ready: true,
+		env: {
+			NODE_ENV: "dev",
+			TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
+			CHAT_ID: process.env.CHAT_ID
+		},
+		env_production: {
+			NODE_ENV: "production",
+			TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN-dev,
+			CHAT_ID: process.env.CHAT_ID-dev
+		}
+	}],
+	deploy: {
+		production: {
+			user: 'git',
+			host: 'github.com',
+			ref: 'origin/master',
+			repo: 'git@github.com:elizabeth-dev/meubot.git',
+			path: '/var/bots/meubot',
+			'post-deploy': 'npm install && pm2 reload process.js --env production'
+		}
+	}
+}
